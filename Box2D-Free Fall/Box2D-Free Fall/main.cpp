f86f96dd6d8c44073ea7156b72ad81d54faf150f
phani srikar <phanisrikar@phanis-MacBook-Air-3.local>
2020-04-10 19:58:43 +0530

    Initilised Box2D and renderd the ground plane physics body using GLFW.

--- Box2D-Free Fall/Box2D-Free Fall/main.cpp
+++ Box2D-Free Fall/Box2D-Free Fall/main.cpp
@@ -12,23 +12,95 @@
 #include <GL/glew.h>
 //GLEW - V3
 #include <GLFW/glfw3.h>
+//Include Box2D here
+#include <box2d/box2d.h>
+
 
 
 #define WIDTH 600
 #define HEIGHT 800
+//Since Box2D uses MKS system hame proper conversion karna hai pixels se meters aour peche se bhi.
+const float M2P=20;
+const float P2M=1/M2P;
+//Define the gravity vector.
+b2Vec2 gravity(0.0f, -5.0f);
+// Construct a world object, which will hold and simulate the rigid bodies.
+b2World world(gravity);
+
+
+// MARK: - Box2D object creation Function
+b2Body* CreateBox2DRect(int x, int y, int hx, int hy, bool isDynamic, int density, int friction)
+{
+     b2BodyDef bodydef;
+     bodydef.position.Set(x*P2M,y*P2M);
+     if(isDynamic)
+             bodydef.type = b2_dynamicBody;
+    b2Body* body = world.CreateBody(&bodydef);
+    
+     b2PolygonShape shape;
+     shape.SetAsBox(P2M*hx/2,P2M*hy/2);
+    
+     b2FixtureDef fixturedef;
+     fixturedef.shape = &shape;
+     fixturedef.density = density;
+    fixturedef.friction = friction;
+     body->CreateFixture(&fixturedef);
+    
+    return body;
+}
+
+
+// MARK: - OpenGl object Rendering Function
+void RenderRect(b2Vec2* points,b2Vec2 center,float angle)//Rendering is done using coordinate data from the box2d engone aur opegl to show the graphics implementation of it.
+{
+    glColor3f(1,1,1);
+        glPushMatrix();
+                glTranslatef(center.x,center.y,0);
+                glRotatef(angle*180.0/M_PI,0,0,1);
+                    glBegin(GL_QUADS);
+                        for(int i=0;i<4;i++)
+                            glVertex2f(points[i].x,points[i].y);
+                glEnd();
+        glPopMatrix();
+}
+
+
+
+// MARK: - Display Function
+void Display()
+{
+    glLoadIdentity();
+    b2Body* tmp = world.GetBodyList();
+    b2Vec2 points[4];
+    while(tmp)
+    {
+        for(int i=0;i<4;i++)
+        {
+             points[i]=((b2PolygonShape*)tmp->GetFixtureList()->GetShape())->m_vertices[i];
+        }
+        //Here we render all the objects from linked lilsts and ge the position from th ephysics engine
+        float angle = tmp->GetWorldCenter().y;
+        std::cout << angle << std::endl;
+        RenderRect(points, tmp->GetWorldCenter(), tmp->GetAngle());
+        tmp=tmp->GetNext();
+    }
+}
+
+
+
+// MARK: - Main Function
+
 
 int main(){
-    
+    // MARK: - OpenGL Initiliasation and Setup
+
     //Initialise glfw here
     glfwInit();
     
     
-    //chalo now let's configure GLFW here
+    //chalo now let's init GLFW here
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
     
     
     //now time to create the window
@@ -63,26 +135,49 @@ int main(){
           std::cout << "Failed to initialize GLEW" << std::endl;
           return -1;
     }
+
     
      glViewport(0, 0, screenWidth, screenHeight);
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
     
-    // Game loop
-    while (!glfwWindowShouldClose(window))
-    {
+    
+    
+    // MARK: - Box2D Physics Bodies Initilialisation
+    //Ground Plane
+    CreateBox2DRect(0, -10, 50, 10, false, 1, 0.2);
+
+    
+    // MARK: - Game Loop
+    
+    
+   while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+    glfwWindowShouldClose(window) == 0 ){
            // Check if any events have been activiated (key pressed,
            //mouse moved etc.) and call corresponding response functions
-           glfwPollEvents();
 
-        glClearColor(1, 1, 1, 1)
+        glClearColor(0, 0, 0, 1);
         glClear(GL_COLOR_BUFFER_BIT);
      
-          // Draw Here
-        
+       
+       //  Instruct the world to perform a single step of simulation.
+       //         It is generally best to keep the time step and iterations fixed.
+               float timeStep = 1.0f / 60.0f;
+               int32 velocityIterations = 6;
+               int32 positionIterations = 2;
+       
+               world.Step(timeStep, velocityIterations, positionIterations);//Physics Update
+// MARK: Render Graphics and Update Physics here
+       Display();
      
+       
+       
+       
      
           glfwSwapBuffers(window);//IDK why this is used.
         //This function swaps the front and back buffers of the specified window. If the swap interval is greater than zero, the GPU driver waits the specified number of screen updates before swapping the buffers.
         //https://www.glfw.org/docs/3.0/group__context.html#ga15a5a1ee5b3c2ca6b15ca209a12efd14
+       glfwPollEvents();
+
     }
     glfwTerminate();
 
